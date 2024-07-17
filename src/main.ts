28c1779cd386790bdb7e777eb8ec2025ce166b94
prince <prince.oncada@gmail.com>
2024-07-17 16:58:06 -0400

    port set

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, "0.0.0.0");
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
