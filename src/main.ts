3af57d6f40d355ed6a07e3c8aa82eebca70f5261
prince <prince.oncada@gmail.com>
2024-07-17 16:29:42 -0400

    port added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+require('dotenv').config()
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -10,7 +11,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
